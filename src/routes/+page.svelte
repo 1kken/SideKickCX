1712ec239368694e2876694ea90d35d4facd52de
ikkenzi <tavorearljohn@gmail.com>
2025-05-17 17:58:24 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	``;
+</script>
+
+<Button>Click me</Button>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>Card Title</Card.Title>
+		<Card.Description>Card Description</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<p>Card Content</p>
+	</Card.Content>
+	<Card.Footer>
+		<p>Card Footer</p>
+	</Card.Footer>
+</Card.Root>
